1ab770cad8d81712908d66f6901698bb0d176122
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-12-11 13:09:37 -0500

    Internationalisation triviale

---
+++ src/main/java/com/olivierlafleur/lab2/client/HelloWorldConstants.java
@@ -0,0 +1,8 @@
+package com.olivierlafleur.lab2.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface HelloWorldConstants extends Constants {
+	public String Hello();
+	public String Farewell();
+}
--- src/main/java/com/olivierlafleur/lab2/client/ProjectEntryPoint.java
+++ src/main/java/com/olivierlafleur/lab2/client/ProjectEntryPoint.java
@@ -1,6 +1,7 @@
 package com.olivierlafleur.lab2.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -11,7 +12,9 @@ public class ProjectEntryPoint implements EntryPoint {
   
   @Override
   public void onModuleLoad() {
-    RootPanel.get().add(new HTML("GWT App has loaded."));
+    HelloWorldConstants constants = GWT.create(HelloWorldConstants.class);
+    
+    RootPanel.get().add(new HTML(constants.Hello()));
   }
 
 }
