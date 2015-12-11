e86375d7282a0a8105cf36ff759120bcc40ef6e2
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-12-11 14:32:51 -0500

    Client-side AJAX

--- src/main/java/com/olivierlafleur/lab2/client/ProjectEntryPoint.java
+++ src/main/java/com/olivierlafleur/lab2/client/ProjectEntryPoint.java
@@ -6,9 +6,17 @@ import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TabPanel;
 
 /**
@@ -23,9 +31,11 @@ public class ProjectEntryPoint implements EntryPoint {
     
     RootPanel.get().add(new HTML(constants.Hello()));
   
+    SimplePanel retour = requeteServeur();
+    
     tabPanel = new TabPanel();
     
-    tabPanel.add(new HTML("<h1>Page 0</h1>"), "Page 0");
+    tabPanel.add(retour, "Page 0");
     tabPanel.add(new HTML("<h1>Page 1</h1>"), "Page 1");
     tabPanel.add(new HTML("<h1>Page 2</h1>"), "Page 2");
     
@@ -56,6 +66,37 @@ public class ProjectEntryPoint implements EntryPoint {
     tabPanel.selectTab(0);
     
     RootPanel.get().add(tabPanel);
+  }
+  
+  private SimplePanel requeteServeur() {
+	  RequestBuilder rb = new RequestBuilder(RequestBuilder.GET, "http://127.0.0.1:8888");
+	  final Label label = new Label();
+	  SimplePanel simplePanel = new SimplePanel();
+	  
+	  rb.setCallback(new RequestCallback() {
+		  public void onResponseReceived (Request req, Response res) {
+			  if(res.getStatusCode() == 200) {
+				  label.setText(res.getText());
+			  } else {
+				  label.setText("Erreur");
+			  }
+		  }
+		  
+		  public void onError(Request req, Throwable e) {
+			  label.setText("ERREUR SERVEUR");
+		  }
+	  });
+	  
+	  try {
+		rb.send();
+	} catch (RequestException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	  
+	  simplePanel.add(label);
+	  
+	  return simplePanel;
   }
 
 }
